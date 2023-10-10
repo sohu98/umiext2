3cf7f63eddce8c7ff08f35246e8d14227c3f718f
sohu98 <winningelevenfts@hotmail.com>
2023-10-10 16:58:54 +0800

    Update t_sql.php

--- t_sql.php
+++ t_sql.php
@@ -4,21 +4,10 @@ print("test 3");
 
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:xxmtestdb.database.windows.net,1433; Database = ext2db", "xxmtestdb_admin", "{$RFV6yhn}");
+    $conn = new PDO("sqlsrv:server = tcp:xxmtestdb.database.windows.net,1433; Database = ext2db", "xxmtestdb_admin", "$RFV6yhn");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 echo "Step1 OK \n";
-    $tsql = "SELECT @@Version AS SQL_VERSION";
-    $stmt = sqlsrv_query($conn, $tsql);
-    if ($stmt === false) {
-        echo "Failed to run the simple query (system-assigned).\n";
-        print_r(sqlsrv_errors());
-    } else {
-        while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-            echo $row['SQL_VERSION'] . PHP_EOL;
-        }
-        sqlsrv_free_stmt($stmt);
-    }    
-    sqlsrv_close($conn);
+   
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
