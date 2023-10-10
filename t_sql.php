221264b7ea79a203b49dddef44830bcdb971db85
sohu98 <winningelevenfts@hotmail.com>
2023-10-10 16:52:58 +0800

    Update t_sql.php

--- t_sql.php
+++ t_sql.php
@@ -2,7 +2,7 @@
 
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:xxmtestdb.database.windows.net,1433; Database = ext2db", "xxmtestdb_admin", "$RFV6yhn");
+    $conn = new PDO("sqlsrv:server = tcp:xxmtestdb.database.windows.net,1433; Database = ext2db", "xxmtestdb_admin", "{$RFV6yhn}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 echo "Step1 OK \n";
     $tsql = "SELECT @@Version AS SQL_VERSION";
