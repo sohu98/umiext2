b0d757f1cc8a1809f69db8e6b22aeb0b90f6337e
sohu98 <winningelevenfts@hotmail.com>
2023-10-10 16:50:34 +0800

    Create t_id.php

---
+++ t_id.php
@@ -0,0 +1,30 @@
+<?php
+
+$azureServer = 'myazureserver.database.windows.net';
+$azureDatabase = 'myazuredatabase';
+$connectionInfo = array('Database'=>$azureDatabase,
+                        'Authentication'=>'ActiveDirectoryMsi');
+$conn = sqlsrv_connect($azureServer, $connectionInfo);
+
+if ($conn === false) {
+    echo "Could not connect with Authentication=ActiveDirectoryMsi (system-assigned).\n";
+    print_r(sqlsrv_errors());
+} else {
+    echo "Connected successfully with Authentication=ActiveDirectoryMsi (system-assigned).\n";
+    
+    $tsql = "SELECT @@Version AS SQL_VERSION";
+    $stmt = sqlsrv_query($conn, $tsql);
+    if ($stmt === false) {
+        echo "Failed to run the simple query (system-assigned).\n";
+        print_r(sqlsrv_errors());
+    } else {
+        while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+            echo $row['SQL_VERSION'] . PHP_EOL;
+        }
+
+        sqlsrv_free_stmt($stmt);
+    }
+    
+    sqlsrv_close($conn);
+}
+?>
