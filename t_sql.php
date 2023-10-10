1daff986a8377b385465ee245803c7da0b467e70
sohu98 <winningelevenfts@hotmail.com>
2023-10-10 17:14:26 +0800

    Update t_sql.php

--- t_sql.php
+++ t_sql.php
@@ -12,12 +12,13 @@ $serverName = "xxmtestdb.database.windows.net"; // update me
     );
     //Establishes the connection
     $conn = sqlsrv_connect($serverName, $connectionOptions);
-        $tsql = "SELECT @@Version AS SQL_VERSION";
+    $tsql = "SELECT @@Version AS SQL_VERSION";
     $stmt = sqlsrv_query($conn, $tsql);
     if ($stmt === false) {
         echo "Failed to run the simple query (system-assigned).\n";
         print_r(sqlsrv_errors());
     } else {
+        echo "conn ok";
         while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
             echo $row['SQL_VERSION'] . PHP_EOL;
         }
