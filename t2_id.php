7ce5f4bbcaae5d64d1cd85b2432c36b66f8c861a
sohu98 <winningelevenfts@hotmail.com>
2023-10-10 21:17:49 +0800

    Update t2_id.php

--- t2_id.php
+++ t2_id.php
@@ -1,1 +1,34 @@
+<?php
 
+echo "test 21";
+
+$azureServer = 'xxmtestdb.database.windows.net';
+$azureDatabase = 'ext2db';
+$umi = '29cd668f-a459-4a0d-a6be-7ce9bf67e94e';
+$connectionInfo = array('Database'=>$azureDatabase,
+                        'UID' => $umi,
+                        'Authentication'=>'ActiveDirectoryMsi');
+$conn = sqlsrv_connect($azureServer, $connectionInfo);
+
+if ($conn === false) {
+    echo "Could not connect with Authentication=ActiveDirectoryMsi (system-assigned).\n";
+    print_r(sqlsrv_errors());
+} else {
+    echo "Connected successfully with Authentication=ActiveDirectoryMsi (system-assigned).\n";
+    
+    $tsql = "SELECT @@Version AS SQL_VERSION";
+    $stmt = sqlsrv_query($conn, $tsql);
+    if ($stmt === false) {
+        echo "Failed to run the simple query (system-assigned).\n";
+        print_r(sqlsrv_errors());
+    } else {
+        while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+            echo $row['SQL_VERSION'] . PHP_EOL;
+        }
+
+        sqlsrv_free_stmt($stmt);
+    }
+    
+    sqlsrv_close($conn);
+}
+?>
